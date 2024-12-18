268a2c6789cb5cb1c9fc2d09c71d2a7f1c854359
Le Xuan Hoang <88936785+LaiLaK918@users.noreply.github.com>
2024-12-18 10:19:35 +0700

    update scaler

--- utils/scalers.py
+++ utils/scalers.py
@@ -22,10 +22,9 @@ def scale_df_with_scalar_multiplication(df: pd.DataFrame, target_min: int, targe
             col_max = scaled_df[col].max()
 
             # Calculate the scaling factor
-            scalar = (target_max - target_min) / \
-                (col_max - col_min) if col_max != col_min else 1
+            scalar = target_max / col_max if col_max != 0 else 1
             scalars.append(scalar)
 
             # Scale values by multiplication and round to integer
-            scaled_df[col] = (scaled_df[col] * scalar).round().astype(int)
+            scaled_df[col] = (scaled_df[col] * scalar).clip(lower=target_min, upper=target_max).round().astype(int)
     return scaled_df, scalars
